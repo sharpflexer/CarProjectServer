f752aad578fb7aa5a1821b9d5f7d9a132a9c3e98
c.sharp.proger <c.sharp.proger@outlook.com>
2024-01-17 18:53:22 +0300

    Комменты

--- CarProjectServer.API/Controllers/NotificationController.cs
+++ CarProjectServer.API/Controllers/NotificationController.cs
@@ -1,44 +1,68 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Net.WebSockets;
 using System.Text;
 
 namespace CarProjectServer.API.Controllers
 {
+    /// <summary>
+    /// Контроллер веб-сокета оповещений.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        private delegate Task MessageHandler(byte[] message, WebSocket webSocket);
+        private static event MessageHandler NotifyByAdmin;
+
+        /// <summary>
+        /// Подключение по веб-сокету
+        /// </summary>
+        /// GET api/notification/ws
         [HttpGet("ws")]
         public async Task Get()
         {
-            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
             NotificationTimer.Notify += MessageHandler;
+
             await Echo(webSocket);
-            
+
             async Task MessageHandler(string message)
             {
-                var bytes = Encoding.UTF8.GetBytes(message);
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
 
                 await webSocket.SendAsync(
                     bytes, WebSocketMessageType.Text, true, CancellationToken.None);
             }
-            
+        }
+
+        [HttpGet("notifyAll")]
+        public async Task NotifyAllAsync()
+        {
+            using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            NotifyByAdmin += MessageHandler;
+
+            await Echo(webSocket);
+
+            async Task MessageHandler(byte[] message, WebSocket sender)
+            {
+                var reciever = webSocket;
+                if (reciever != sender)
+                {
+                    await webSocket.SendAsync(
+                        message, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
         }
 
         private static async Task Echo(WebSocket webSocket)
         {
-            var buffer = new byte[1024 * 4];
-            var receiveResult = await webSocket.ReceiveAsync(
+            byte[] buffer = new byte[1024 * 4];
+            WebSocketReceiveResult receiveResult = await webSocket.ReceiveAsync(
                 new ArraySegment<byte>(buffer), CancellationToken.None);
 
             while (!receiveResult.CloseStatus.HasValue)
             {
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
-                    receiveResult.MessageType,
-                    receiveResult.EndOfMessage,
-                    CancellationToken.None);
+                NotifyByAdmin.Invoke(buffer, webSocket);
 
                 receiveResult = await webSocket.ReceiveAsync(
                     new ArraySegment<byte>(buffer), CancellationToken.None);
